9c40e6ddc9b87e8ce041b59869363f897526dba0
Daniel Sanchez <dansanchezbui@unal.edu.co>
2021-04-03 23:51:58 -0500

    Gran commit inicial

---
+++ bot.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.BOT_TOKEN;
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+names = require('./names');
+
+let anterior = -1;
+
+// Matches "/echo [whatever]"
+bot.onText(/.*\b([pP][rR][eE][sS][iI][oOóÓ][nN]).*/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    let idx;
+    do {
+        idx = Math.floor(Math.random() * (names.length - 1));
+    } while (idx == anterior);
+
+    // send back the matched "whatever" to the chat
+    //bot.sendMessage(chatId, resp);
+    const path = './img/' + names[idx];
+    bot.sendPhoto(chatId, path);
+    if (names[idx] == 'corviknight.jfif') {
+        bot.sendPhoto(chatId, './img/corviknight2.jfif');
+    }
+});
