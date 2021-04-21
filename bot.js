7c8ff533a7c781905438bfd33283bdda23bef610
Daniel Sanchez <dansanchezbui@unal.edu.co>
2021-04-21 10:54:43 -0500

    Pequeñas mejoras y nuevas imágenes

--- bot.js
+++ bot.js
@@ -1,17 +1,14 @@
+// Configuración de librerías
 require('dotenv').config();
-
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.BOT_TOKEN;
-
-const bot = new TelegramBot(token, { polling: true });
-
+// Creación de bot y parámetros
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 const names = require('./names');
-
 let anterior = -1;
 let idx;
 
-// Si alguien dice una palabra que empiece con "presión", el bot envía una imagen como respuesta
+// Si alguien dice una palabra que empiece con "presion", el bot envía una imagen como respuesta
 bot.onText(/.*\b([pP][rR][eE][sS][iI][oOóÓ][nN]).*/, (msg, match) => {
     const chatId = msg.chat.id;
 
@@ -19,17 +16,28 @@ bot.onText(/.*\b([pP][rR][eE][sS][iI][oOóÓ][nN]).*/, (msg, match) => {
         idx = Math.floor(Math.random() * (names.length - 1));
     } while (idx == anterior);
 
-    const path = './img/' + names[idx];
-    const options = {
-        reply_to_message_id: msg.message_id
-    }
-    const fileOptions = {
-        contentType: 'image/' + path.split('.')[1]
-    }
-    bot.sendPhoto(chatId, path, options, fileOptions);
+    let rep = bot.sendPhoto(
+        chatId,
+        './img/' + names[idx],
+        {
+            reply_to_message_id: msg.message_id
+        },
+        {
+            contentType: 'image/' + path.split('.')[1]
+        }
+    );
 
     if (names[idx] == 'corviknight.jfif') {
-        bot.sendPhoto(chatId, './img/corviknight2.jfif');
+        bot.sendPhoto(
+            chatId,
+            './img/corviknight2.jfif',
+            {
+                reply_to_message_id: rep.message_id
+            },
+            {
+                contentType: 'image/jfif'
+            }
+        );
     }
 
     anterior = idx;
